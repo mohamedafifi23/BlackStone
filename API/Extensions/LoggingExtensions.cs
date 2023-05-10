8d2c3d74c23682698aa510108b728d7f1651ee2c
mohamedafifi23 <127197190+mohamedafifi23@users.noreply.github.com>
2023-05-10 08:22:20 +0300

    serlilog

---
+++ API/Extensions/LoggingExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using System.Diagnostics;
+
+namespace API.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static void AddLogger(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((ctx, loggerConfiguration) =>
+            {
+                loggerConfiguration
+                    .ReadFrom.Configuration(ctx.Configuration)
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                    .Enrich.WithProperty("Environment", ctx.HostingEnvironment);
+
+            #if DEBUG
+                // Used to filter out potentially bad data due debugging.
+                // Very useful when doing Seq dashboards and want to remove logs under debugging session.
+                loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
+            #endif
+            });
+        }
+    }
+}
