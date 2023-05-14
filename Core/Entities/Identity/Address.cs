bb0df1a7d651f067f8d4edf016cc730350588743
mohamedafifi23 <127197190+mohamedafifi23@users.noreply.github.com>
2023-05-14 22:04:27 +0300

    fixing HostAbortedException

--- Core/Entities/Identity/Address.cs
+++ Core/Entities/Identity/Address.cs
@@ -14,6 +14,7 @@ namespace Core.Entities.Identity
 
         public string LastName { get;set; }
 
+        public string Country { get;set; }    
         public string State { get;set; }    
 
         public string City { get;set; }
